fceec10d22e883837619b9e1d2ea1a8c1c5be0dd
Abhinav <abhinav.rajan0@gmail.com>
2012-09-28 16:28:17 +0800

    change call back implementation

--- rmi-base/src/com/ds/maze/Notify.java
+++ rmi-base/src/com/ds/maze/Notify.java
@@ -1,8 +1,9 @@
 package com.ds.maze;
 
 import java.rmi.*;
+import java.util.HashMap;
 
 public interface Notify extends Remote {
-    public void onSuccess() throws RemoteException;
-    public void onFailure(String crashedClient) throws RemoteException;
+    public void onSuccess(HashMap<String,Object> gameState) throws RemoteException;
+    public void onFailure(String crashedClient,HashMap<String,Object> gameState) throws RemoteException;
 }

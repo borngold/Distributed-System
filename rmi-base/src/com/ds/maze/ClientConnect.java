b06d746b372f48cfc9604e0caf3ce3229e886bbe
abhinav-sunderrajan <abhinav.rajan0@gmail.com>
2012-10-01 13:38:23 +0800

    make call back serializable

--- rmi-base/src/com/ds/maze/ClientConnect.java
+++ rmi-base/src/com/ds/maze/ClientConnect.java
@@ -1,10 +1,11 @@
 package com.ds.maze;
 
+import java.io.Serializable;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 
-public interface ClientConnect extends Remote{
+public interface ClientConnect extends Remote, Serializable{
 	 public void onSuccess(HashMap<String,Object> gameState) throws RemoteException;
 	   public void onFailure() throws RemoteException;
 
